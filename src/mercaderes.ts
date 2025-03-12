9c81d48eedcf64d28033da3debde7bd0e97bbd0d
Javier Gonzalez <alu0101548197@ull.edu.es>
2025-03-12 10:41:01 +0000

    Mercader

--- src/mercaderes.ts
+++ src/mercaderes.ts
@@ -1,1 +1,14 @@
 // mercaderes.ts
+
+export enum TraderTypes {
+  Blacksmith,
+  Alchemist,
+  GeneralMerchant
+}
+
+export interface Trader {
+  id: number,
+  name: string,
+  type: TraderTypes,
+  location: string
+}
