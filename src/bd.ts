f00742e6a0a71a1cc9414f9317e88cf023af72cd
IPG04 <alu0101543345@ull.edu.es>
2025-03-13 17:18:42 +0000

    Inventary and DB

--- src/bd.ts
+++ src/bd.ts
@@ -1,18 +1,13 @@
 // db.ts
-import { Low, LowSync } from 'lowdb';
-import { JSONFile, JSONFileSync } from 'lowdb/node';
-
-export interface DataEntry<Data> {
-  key: number;
-  value: Data;
-}
+import { LowSync } from 'lowdb';
+import { JSONFileSync } from 'lowdb/node';
 
 export interface Database<T> {
-  data: DataEntry<T>[];
+  data: T[];
 }
 
-export class GenericDatabase<T> {
-  private _db: LowSync<Database<T>>;
+export abstract class GenericDatabase<T> {
+  protected _db: LowSync<Database<T>>;
 
   constructor(dbFile: string = './db/db.json') {
     const adapter = new JSONFileSync<Database<T>>(dbFile);
@@ -21,85 +16,20 @@ export class GenericDatabase<T> {
     this._db.read();
   }
 
-  filterEntry(key: number): T | undefined {
-    const result : T | undefined = this._db.data.data.find((val: DataEntry<T>) => val.key === key)?.value
-    return result;
-  }
-
-  addEntry(key: number, value: T): void {
-    if (this.filterEntry(key) != undefined) {
-      throw new Error('Cannot insert value in database: Value alredy in');
+  addEntry(value: T): void {
+    if (this.filterEntry(value) != undefined) {
+      throw new Error('Value not inserted. Value alredy in table.')
     }
-    this._db.data.data.push({ key, value });
+    this._db.data.data.push(value);
     this._db.write();
   }
 
-  deleteEntry(key: number): void {
-    const result = this._db.data.data.filter((val: DataEntry<T>) => val.key !== key);
-    this._db.data.data = result;
-    this._db.write();
+  getAllEntries(): void {
+    console.table(this._db.data.data);
   }
 
-  getAllEntries(): DataEntry<T>[] {
-    return this._db.data.data;
-  }
+  abstract findValues(filter: {}): T[];
+  abstract filterEntry(value: T): T | undefined;
+  abstract deleteEntry(filter: {}): void;
+  abstract modifyEntry(filter: {}): void;
 }
-
-const db = new GenericDatabase<string>();
-
-
-// import { TodoItem } from "./todoItem.js";
-// import { TodoCollection } from "./todoCollection.js";
-// import { LowSync } from "lowdb";
-// import { JSONFileSync } from "lowdb/node";
-// type schemaType = {
-// tasks: { id: number; task: string; complete: boolean; }[]
-// };
-// export class JsonTodoCollection extends TodoCollection {
-// private database: LowSync<schemaType>;
-// constructor(public userName: string, todoItems: TodoItem[] = []) {
-// super(userName, []);
-// this.database = new LowSync(new JSONFileSync("Todos.json"));
-// this.database.read();
-// if (this.database.data == null) {
-// this.database.data = { tasks : todoItems};
-// this.database.write();
-// todoItems.forEach(item => this.itemMap.set(item.id, item));
-// } else {
-// this.database.data.tasks.forEach(item =>
-// this.itemMap.set(item.id,
-// new TodoItem(item.id, item.task, item.complete)));
-// }
-// }
-// addTodo(task: string): number {
-// let result = super.addTodo(task);
-// this.storeTasks();
-// return result;
-// }
-// markComplete(id: number, complete: boolean): void {
-// super.markComplete(id, complete);
-// this.storeTasks();
-// }
-// removeComplete(): void {
-// super.removeComplete();
-// this.storeTasks();
-// }
-// private storeTasks() {
-// this.database.data.tasks = [...this.itemMap.values()];
-// this.database.write();
-// }
-
-
-// import { TodoItem } from "./todoItem.js";
-// type ItemCounts = {
-// total: number,
-// incomplete: number
-// }
-// export class TodoCollection {
-// private nextId: number = 1;
-// protected itemMap = new Map<number, TodoItem>();
-// constructor(public userName: string, todoItems: TodoItem[] = []) {
-// todoItems.forEach(item => this.itemMap.set(item.id, item));
-// }
-// // ...methods omitted for brevity...
-// }
